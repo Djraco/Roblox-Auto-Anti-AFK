0ffa1e7e605119279c26c8d91a940a311a3d2589
Djraco <josh@cecka.org>
2023-08-20 06:48:09 -0700

    small tweaks & added logging for loader.py

--- loader.py
+++ loader.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, simpledialog
 import threading
+import logging
 
 # Global variables
 message_box = None
@@ -9,6 +10,8 @@ timeout_event = None
 countdown_var = None
 running = True
 countdown_triggered_by_button = False
+log_file = "Logs/Loader/latest.txt"
+logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Customizable variables
 countdown_time = 30
@@ -18,6 +21,10 @@ window_height = 140
 # Initialize main window
 root = tk.Tk()
 root.withdraw()  # Hide the main window
+
+def clear_log_file(file_path):
+    with open(file_path, "w") as file:
+        pass
 
 def calculate_message_box_position():
     screen_width = root.winfo_screenwidth()
@@ -38,7 +45,6 @@ def create_message_box(title, message, buttons, show_close_button=True):
     message_box.title(title)
     message_box.overrideredirect(1)
 
-    # Add border and relief
     message_box.configure(borderwidth=4, relief=tk.RIDGE)
 
     style = ttk.Style()
@@ -82,44 +88,52 @@ def on_cancel_button_click():
 
     if minutes is not None:
         running = True
-        print("Reprompting in {} minute{}".format(minutes, ("s" if minutes != 1 else "")))
+        logging.info("Reprompting in {} minute{}".format(minutes, ("s" if minutes != 1 else "")))
         root.after((minutes * 60 * 1000) if minutes != 1010 else 1000, start_message_box)
 
 def start_afk():
-    print("Starting AFK...")
+    logging.info("Starting AFK...")
 
 def update_countdown(i):
     global countdown_triggered_by_button
     if i > 0 and running:
         countdown_var.set(f"Starting AFK in {i}...")
-        print(f"Countdown: {i}")
+        logging.info(f"Countdown: {i}")
         root.after(1000, update_countdown, i - 1)
     else:
         countdown_var.set("Starting AFK now...")
         if running and i == 0 and not countdown_triggered_by_button:
-            print("Countdown finished, calling start_afk() from update_countdown")
+            logging.info("Countdown finished, calling start_afk() from update_countdown")
             start_afk()
             countdown_triggered_by_button = True
             on_start_button_click()
         else:
-            print("Countdown stopped or not at the end")
+            logging.info("Countdown stopped or not at the end")
 
 def timeout_callback():
     update_countdown(countdown_time)
 
 def ask_minutes():
+    logging.info("Asking how long until reprompt")
     minutes = simpledialog.askinteger("Auto Anti AFK", "Enter the number of minutes until reprompting:", parent=root, minvalue=1)
     if minutes is None:
+        logging.info("Canceled")
         root.quit()
     return minutes
 
 def start_message_box():
-    create_message_box("Auto Anti AFK", f"Starting AFK in {countdown_time}...", [("Start now", on_start_button_click), ("Cancel", on_cancel_button_click)], show_close_button=False)
+    logging.info("Spawning message box")
+    
+    try:
+        create_message_box("Auto Anti AFK", f"Starting AFK in {countdown_time}...", [("Start now", on_start_button_click), ("Cancel", on_cancel_button_click)], show_close_button=False)
+    except:
+        logging.error("Error spawning message box.")
 
     x, y = calculate_message_box_position()
     message_box.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
-# Start the initial message box
+clear_log_file(log_file)
+
 start_message_box()
 
 root.mainloop()
